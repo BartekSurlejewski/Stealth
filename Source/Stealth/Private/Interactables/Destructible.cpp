e008d6cfbfbed0d683b8a0d461e480e0991dba12
BartekSurlejewski <bartoszsurlejewski@gmail.com>
2026-08-26 11:11:38 +0200

    Added IDestructible interface

---
+++ Source/Stealth/Private/Interactables/Destructible.cpp
@@ -0,0 +1,7 @@
+﻿// 
+
+
+#include "Interactables/Destructible.h"
+
+
+// Add default functionality here for any IDestructible functions that are not pure virtual.
---
+++ Source/Stealth/Public/Interactables/Destructible.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Destructible.generated.h"
+
+UINTERFACE(MinimalAPI, BlueprintType)
+class UDestructible : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class STEALTH_API IDestructible
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Destructible")
+	void Destroy();
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Destructible")
+	void Fix();
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Destructible")
+	bool WasDestroyed();
+};
